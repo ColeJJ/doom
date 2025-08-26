b2673631c63ad0805f262cef4a827b01ef09f032
ColeJJ <torben.unland@gmail.com>
2025-08-26 08:48:39 +0200

    init

---
+++ custom.el
@@ -0,0 +1,15 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("a939897b56010ef16d737b3a145ab3f935e0da6122ded3bf9cad28f88b09fd68" default))
+ '(package-selected-packages '(@ gruber-darker-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
